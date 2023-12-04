6d0cbf488a18bbdb7935340008ddfb7dbc1038d3
Tobias Müller <tmueller@easy2parts.com>
2023-12-04 11:57:32 +0100

    Add Day3 Template

---
+++ Day3/Day3/Day3.cs
@@ -0,0 +1,10 @@
+﻿namespace Day3;
+
+public static class Day3
+{
+    public static int Day3_1(string[] input)
+    {
+        
+        return 0;
+    }
+}
---
+++ Day3/Day3Test/Day3Test.cs
@@ -0,0 +1,16 @@
+namespace Day3Test;
+
+public class Day3Test
+{
+    [Fact]
+    public void TestPartOne()
+    {
+        var result = Day3.Day3.Day3_1(File.ReadAllLines("testInput.txt"));
+    }
+
+    [Fact]
+    public void TestPartTwo()
+    {
+        
+    }
+}
